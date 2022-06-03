8c82bc8e0f58db598570b4395728eb66b3a493b3
Lucas Moreira <Lucas_moreira@outlook.com>
2022-06-03 13:10:44 -0300

    Issue #3 | Relatórios e documentos
    Formulario e report criados

---
+++ FormDocumentos.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Data;
+using MySql.Data.MySqlClient;
+using System.Windows.Forms;
+
+namespace ANAYA_VIAGENS
+{
+    public partial class FormDocumentos : Form
+    {
+        public FormDocumentos()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            FormRelatorioTransporte formRelatorioTransporte = new FormRelatorioTransporte();
+            formRelatorioTransporte.Show();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            CRUD.sql = "SELECT * FROM CLIENTES";
+
+            CRUD.cmd = new MySqlCommand(CRUD.sql, CRUD.con);
+            DataTable dt = CRUD.PerformCRUD(CRUD.cmd);
+
+
+            DataGridView dgv = dataGridView1;
+
+            dgv.MultiSelect = true;
+            dgv.AutoGenerateColumns = false;
+            dgv.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dgv.DataSource = dt;
+            dgv.AutoResizeColumns(DataGridViewAutoSizeColumnsMode.AllCells);
+        }
+    }
+}
---
+++ FormRelatorioTransporte.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ANAYA_VIAGENS
+{
+    public partial class FormRelatorioTransporte : Form
+    {
+        public FormRelatorioTransporte()
+        {
+            InitializeComponent();
+        }
+
+        private void RelatorioTransporte_Load(object sender, EventArgs e)
+        {
+
+            this.reportViewer1.RefreshReport();
+        }
+    }
+}
